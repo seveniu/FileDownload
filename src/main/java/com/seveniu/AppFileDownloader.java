28e973051957e3d78c20d334f5a93c5d82c9b2f2
seveniu <prime3721@gmail.com>
2016-08-01 01:07:36 +0800

    fix log

--- src/main/java/com/seveniu/AppFileDownloader.java
+++ src/main/java/com/seveniu/AppFileDownloader.java
@@ -21,18 +21,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
 @ComponentScan("com.seveniu")
 public class AppFileDownloader {
     private static final Logger logger = LoggerFactory.getLogger(AppFileDownloader.class);
